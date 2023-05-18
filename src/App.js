827035a1dd3876abcfbab281496be9634ac29f43
nam3dan <namillennial@gmail.com>
2023-05-17 21:57:46 -0500

    I don't care anymore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight:300,date:'5-17-2023'},{weight:290,date:'5-18-2023'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
 
     </div>
   );
